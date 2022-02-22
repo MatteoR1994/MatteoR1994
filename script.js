9a932f06944b5ebecfd946b8f692e2a5d3952178
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-22 17:28:03 +0100

    aggiornata anagrafica pc e mobile

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function myFunction() {
-    let prop = document.getElementById("nav-menu-div").style.display;
-    if (prop === "none") {
-        document.getElementById("nav-menu-div").style.display = "block";
+    let prop = document.getElementById('nav-menu-div').style.display;
+    if (prop === 'none') {
+        document.getElementById('nav-menu-div').style.display = 'block';
     } else {
-        document.getElementById("nav-menu-div").style.display = "none";
+        document.getElementById('nav-menu-div').style.display = 'none';
     }
-  // document.getElementById("nav-menu-div").style.display = "block";
 }
